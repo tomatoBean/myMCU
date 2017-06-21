81cb5b401841d65c321b88d52c0776a8fe901fdd
demoname <258576082@qq.com>
2017-06-21 11:35:04 +0800

    adding new project demo as helloworld

---
+++ stm32/helloworld/classDemo/Objects/stm32_eval.d
@@ -0,0 +1,2 @@
+.\objects\stm32_eval.o: user\stm32_eval.c
+.\objects\stm32_eval.o: user\stm32_eval.h
---
+++ stm32/helloworld/classDemo/Objects/system_stm32f10x_1.d
@@ -0,0 +1,5 @@
+.\objects\system_stm32f10x_1.o: cmsis\system_stm32f10x.c
+.\objects\system_stm32f10x_1.o: cmsis\stm32f10x.h
+.\objects\system_stm32f10x_1.o: cmsis\core_cm3.h
+.\objects\system_stm32f10x_1.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_stm32f10x_1.o: cmsis\system_stm32f10x.h
